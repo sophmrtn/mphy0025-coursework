a762ffe1e460aed703ce931db7b4dbe930af8b17
Sophie <sophiedarrouxmartin@gmail.com>
2021-03-26 11:47:11 +0000

    Generalises visualisation function for image + contours

--- data.py
+++ data.py
@@ -10,8 +10,8 @@ from python_code_for_coursework_part1 import utilsCoursework
 
 ATLAS_DIR = './head_and_neck_images/atlas'
 TUNE_DIR  =  './head_and_neck_images/tune'
-# Load data from the ATLAS and tuning patients with contours
 
+# Load data from the ATLAS and tuning patients with contours
 def load_image(filepath):
     '''
     Function to load png image from filepath, convert to double and reorientate the images for 
@@ -27,22 +27,46 @@ def load_image(filepath):
     img = np.transpose(img)
     return img
 
-# Get all files in atlas and tune folders
-atlas_files = os.listdir(ATLAS_DIR)
-tune_files = os.listdir(TUNE_DIR)
 
-# load images from atlas and tune folders
-atlas_imgs = [load_image(os.path.join(ATLAS_DIR, f)) for f in atlas_files]
-tune_imgs = [load_image(os.path.join(TUNE_DIR, f)) for f in tune_files]
-print(atlas_files)
+def visualise_image(img_no, directory=ATLAS_DIR, ax=None):
+    '''
+    Function to plot image and contours for a given image number and path to the directory.
 
-# use dispImage utility function to view the CT images
-utilsCoursework.dispImage(load_image('./head_and_neck_images/atlas/atlas_1.png'))
+    Inputs: (int) img_no - integer
+    directory - path to directory containing indexed images
 
-# overlay contours
-plt.contour(np.transpose(load_image('./head_and_neck_images/atlas/atlas_1_BRAIN_STEM.png')),colors='lime', linewidths=0.5)
-plt.contour(np.transpose(load_image('./head_and_neck_images/atlas/atlas_1_SPINAL_CORD.png')), colors='red',  linewidths=0.5)
+    Outputs: none - displays image
+    '''
+    # get basename for filepaths based on directory
+    basename = os.path.basename(directory)
+
+    # get filepaths for images and masks using known patten
+    img_filepath = os.path.join(directory, ('%s_%d.png' % (basename, img_no)))
+    brainstem_mask_filepath = os.path.join(directory, ('%s_%d_BRAIN_STEM.png' % (basename, img_no)))
+    spinal_mask_filepath = os.path.join(directory, ('%s_%d_SPINAL_CORD.png' % (basename, img_no)))
+
+    # use dispImage utility function to view the CT images
+    utilsCoursework.dispImage(load_image(img_filepath), ax=ax)
+
+    # overlay contours: requires taking the transpose due to matplot lib contour function
+    plt.contour(np.transpose(load_image(brainstem_mask_filepath)),colors='lime', linewidths=0.5)
+    plt.contour(np.transpose(load_image(spinal_mask_filepath)), colors='red',  linewidths=0.5)
+
+fig = plt.figure(figsize=(10,8))
+for i in range(1,6):
+    ax = fig.add_subplot(3,2, i)
+    visualise_image(i, ATLAS_DIR, ax=ax)
+    ax.set_title('Patient: %d' % i)
+
+plt.suptitle('ATLAS')
+plt.tight_layout()
+
+fig = plt.figure(figsize=(10,8))
+for i in range(1,4):
+    ax = fig.add_subplot(3,2, i)
+    visualise_image(i, TUNE_DIR, ax=ax)
+    ax.set_title('Patient: %d' % i)
+
+plt.suptitle('TUNE')
+plt.tight_layout()
 plt.show()
-
-
-
